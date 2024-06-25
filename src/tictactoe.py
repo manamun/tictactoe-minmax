993548d23208e5e41f0e914528d473890faeb45e
manamun <satomana0513@gmail.com>
2024-06-26 02:31:22 +0900

    Boardクラス完成

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -28,3 +28,70 @@ class Board:
             return GameManager.DROW
         else:
             return GameManager.ON
+        
+    #現在置くことのできる場所を取得するメソッド
+    def get_possible_moves(self):
+        possible_moves = []
+        for i in range(3):
+            for j in range(3):
+                if self.cell[i][j] == MarkManager.EMPTY:
+                    possible_moves.append((i,j))
+        return possible_moves
+    
+    #指定された場所に印をつけるメソッド
+    def make_mark(self, move):
+
+        if self.cell[move[0]][move[1]] == MarkManager.EMPTY:
+            if self.is_first_player:
+                self.cell[move[0]][move[1]] = MarkManager.X
+            else:
+                self.cell[move[0]][move[1]] = MarkManager.O
+            self.is_is_first_player = not self.is_is_first_player
+
+    #勝利判定メソッド
+    def won(self):
+        def check_cells(x,y,dx,dy):
+            if self.cell[x][y] == MarkManager.EMPTY:
+                return False
+            for i in range(3):
+                if self.cell[x][y] != self.cell[x + i * dx][y + i * dy]:
+                    return False
+            return True
+        
+        for i in range(3):
+            #垂直方向に判定
+            if check_cells(i, 0, 0, 1):
+                return True
+            #水平方向に判定
+            if check_cells(0, i, 1, 0):
+                return True
+        
+        #対角方向に判定
+        if check_cells(0,0,1,1):
+            return True
+        if check_cells(0,2,1,-1):
+            return True
+        
+    #指定されたマスの場所の印を空欄(EMPTY)にするメソッド
+    def rewind(self, move):
+        #rewind the board
+        self.cell[move[0]][move[1]] = MarkManager.EMPTY
+        self.if_first_player = not self.is_is_first_player
+    
+    #盤面を表示するメソッド
+    def __str__(self):
+        board_str = "\n"
+        for i in range(3):
+            for j in range(3):
+                if self.cell[i][j] == MarkManager.X:
+                    board_str += "X"
+                elif self.cell[i][j] == MarkManager.O:
+                    board_str += "O"
+                else:
+                    board_str += "/"
+            board_str += "\n"
+        return board_str
+
+board = Board()
+print(board)
+
